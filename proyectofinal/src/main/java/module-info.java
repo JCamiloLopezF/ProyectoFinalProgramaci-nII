a31eccda9243cc7f299edf3d5230cead9b88ca71
JCamiloLopezF <130943645+JCamiloLopezF@users.noreply.github.com>
2024-05-09 20:11:22 -0500

    Creado proyecto JavaFX

---
+++ proyectofinal/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module co.edu.uniquindio.proyectofinalprii {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens co.edu.uniquindio.proyectofinalprii to javafx.fxml;
+    exports co.edu.uniquindio.proyectofinalprii;
+}
