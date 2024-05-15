d980fa43173ecc38d1094e0c5961e69f07e5dc8e
JCamiloLopezF <130943645+JCamiloLopezF@users.noreply.github.com>
2024-05-14 21:06:16 -0500

    Getters y setters del usuario
    Faltó solo eso

--- proyectofinal/src/main/java/co/edu/uniquindio/proyectofinalprii/Usuario.java
+++ proyectofinal/src/main/java/co/edu/uniquindio/proyectofinalprii/Usuario.java
@@ -13,4 +13,36 @@ public abstract class Usuario {
         this.correo = correo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
 }
